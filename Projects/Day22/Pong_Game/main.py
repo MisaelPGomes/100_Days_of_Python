6e8bc0868da89806ca55e83e3a8c872870a4408d
MisaelPGomes <misapeixoto@gmail.com>
2022-03-19 16:04:04 -0300

    Day 22 - Ping Pong Game

---
+++ Projects/Day22/Pong_Game/main.py
@@ -0,0 +1,69 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+from scoreboard import Scoreboard
+import time
+
+scoreboard = Scoreboard()
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Ping Pong")
+screen.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+ball = Ball()
+
+screen.listen()
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
+
+
+is_game_on = True
+
+while is_game_on:
+    time.sleep(ball.move_speed)
+    screen.update()
+    ball.ball_movement()
+
+    # Detect Collision with Wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with r_paddle
+    if ball.distance(r_paddle) < 60 and ball.xcor() > 340:
+            ball.bounce_x()
+
+
+    # Detect collision with r_paddle
+    if ball.distance(l_paddle) < 60 and ball.xcor() < - 340:
+            ball.bounce_x()
+
+
+    # R Paddle misses ball
+    if ball.xcor() > 380:
+        ball.restart_position()
+        ball.bounce_x()
+        scoreboard.r_point()
+
+
+    # R Paddle misses ball
+    if ball.xcor() < -380:
+        ball.restart_position()
+        ball.bounce_x()
+        scoreboard.l_point()
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
---
+++ Projects/Day22/Pong_Project_Breakdown.md
@@ -0,0 +1,43 @@
+**PONG GAME**
+
+`****BREAKDOWN:`
+
+`<u>`</u>The game have 8 components, which are:
+
+1. Ball
+   
+   - Movimento Aleatório
+   
+   - Mudanção de movimento quando colidir com a raquete
+
+2. ``Raquetes
+   
+   - Movimenta
+   
+   - 
+
+3. []()Scoreboard
+   
+   - Aumenta quando a bola é rebatida pela raquete
+
+4. Table
+   
+   - Marcação central 
+
+PROJECT STRUCTURE
+
+1. Create the Screen
+
+2. Create and move paddle
+
+3. Create another paddle 
+
+4. Create the ball and make it move
+
+5. Detect collision with wall and bounce it 
+
+6. Detect collition with paddle
+
+7. Detect when paddle misses
+
+8. Keep Score       
