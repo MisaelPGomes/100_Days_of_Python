76823f2ee41c06bd1d51f2d45e61eaf837eee746
MisaelPGomes <misapeixoto@gmail.com>
2022-03-21 21:29:55 -0300

    Day24 - Create Invite for many people using

--- Day24/main.py
+++ Day24/main.py
@@ -1,7 +1,5 @@
-#with open("my_file.txt") as file:
-#    contents = file.read()
-#    print(contents)
-#
+with open("../../../Área de Trabalho/my_file.txt") as file:
+   contents = file.read()
+   print(contents)
 
-with open("new_file.txt", mode="w") as file:
-    file.write("\nNew Text.")
+
