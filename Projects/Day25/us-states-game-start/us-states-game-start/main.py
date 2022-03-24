77f7f999947a48a347be0bc535263148b82ec3d2
MisaelPGomes <misapeixoto@gmail.com>
2022-03-24 19:27:53 -0300

    Day 25 - Guess American States Game

---
+++ Projects/Day25/us-states-game-start/us-states-game-start/main.py
@@ -0,0 +1,55 @@
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+game_on = True
+count = 0
+
+guessed_states = []
+study_state = []
+while game_on:
+
+    # Save the typed state and check if it is on the database
+
+    answer_state = (screen.textinput(title=f"Guess a State. {count}/50", prompt="What state do you chose?")).title()
+    estados = pd.read_csv("50_states.csv")
+    all_states = estados["state"].to_list()
+
+    if answer_state == "Exit":
+        for state in all_states:
+            if state not in guessed_states:
+                study_state.append(state)
+
+        break
+
+    for state in all_states:
+
+        if state != answer_state:
+            pass
+        else:
+            chosen_state = estados[estados["state"] == answer_state]
+            count += 1
+            guessed_states.append(state)
+
+            # Consult state location on the map and write it on the map
+
+            x_cord = int(chosen_state["x"])
+            y_cord = int(chosen_state["y"])
+            t = turtle.Turtle()
+            t.penup()
+            t.hideturtle()
+            t.goto(x_cord, y_cord)
+            t.write(answer_state.title())
+
+# Create a List of states that were not guessed and return a csv file
+print(study_state)
+study_list = pd.DataFrame(study_state)
+
+study_list.to_csv("study_list.csv")
+
+# screen.exitonclick()
