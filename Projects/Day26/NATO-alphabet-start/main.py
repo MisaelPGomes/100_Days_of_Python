dff95cc1b131e20fb5ef8af4b98512826f9bc0a0
MisaelPGomes <misapeixoto@gmail.com>
2022-04-01 16:48:22 -0300

    Day30 - Update the Nato project using exceptions: (try, except and else)

--- Projects/Day26/NATO-alphabet-start/main.py
+++ Projects/Day26/NATO-alphabet-start/main.py
@@ -7,14 +7,26 @@ data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato = {row.letter: row.code for (index, row) in data_frame.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-# Imput a word
-user_string = input("Input your phrase: ").upper()
+
+# Condition to run program
+keep_asking = True
+
+while keep_asking:
+# Input a word
+    user_string = input("Input your phrase: ").upper()
 
 # Get string and split it into a list with letters
-list_string = [letter for letter in user_string]
+    list_string = [letter for letter in user_string]
 
-print(list_string)
+    
 # Check the letters and return a list with the correspoding nato value
 
-nato_string = [nato[letter] for letter in list_string if letter !=' ']
-print(nato_string)
+    try:
+        nato_string = [nato[letter] for letter in list_string if letter !=' ']
+
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+
+    else:
+        print(nato_string)
+        keep_asking = False
